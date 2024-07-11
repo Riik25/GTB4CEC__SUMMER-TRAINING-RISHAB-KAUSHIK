8b35480c75500e1b4eb520b3f7ca7bbc1d234a35
Rishab Kaushik <rishabkaushik125@gmail.com>
2024-07-11 14:08:06 +0530

    second commit

--- day1.java
+++ day1.java
@@ -4,3 +4,15 @@ public class day1 {
 
     }
 }
+
+/*
+ * int : integer -> 4 bytes
+ * float: decimal upto 6  bytes
+ * double: decimal upto 15 decimls ->8 bytees
+ * char: character-> 2 bytes
+ * Long: integer -> 8 bytes
+ * boolean: true or false-.1 bit
+ * short: number ->2 bytes
+ */
+
+ 
