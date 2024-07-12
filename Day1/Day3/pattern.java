15a823a561ba9eb1ca4048ec61c4a5a58a99c606
Rishab Kaushik <rishabkaushik125@gmail.com>
2024-07-12 23:52:47 +0530

    commit

---
+++ Day1/Day3/pattern.java
@@ -0,0 +1,165 @@
+package Day1.Day3;
+
+import java.util.Scanner;
+
+public class pattern {
+
+    public static void main(String[] args){
+   //  int n;
+     //   Scanner sc =  new Scanner(System.in);
+     //   n=sc.nextInt();
+       // for (int i=1;i<=n;i++){
+        //for(int j=1;j<=n;j++){
+        //System.out.print("*");
+        //System.out.println();
+        //}
+    //}
+//}
+//}
+
+//int n;
+//Scanner sc =  new Scanner(System.in);
+//n=sc.nextInt();
+//for(int i=1; i<=n; i++){
+  //  for (int k=1; k<=(n-i); k++)
+    //System.out.print("  ");
+    //for(int j=1; j<=i; j++);
+    //System .out.print(" * ");
+    //System.out.println();}
+//}
+
+//}
+
+
+
+/*int n=5;
+for (int i=0;i<n;i++){
+    for (int j=0;j<n;j++){
+        if(i==0||i==n-1||j==0||j==n-1){
+        System.out.print(" *");
+    } else {
+        System.out.print("  ");
+    }
+}
+        System.out.println();
+    }
+
+    
+}
+}/* /* */
+
+
+/*int n=5;
+for(int i=1;i<=n;i++){
+    for(int k=0; k<(n-i); k++){
+        System.out.print("  ");
+    }
+    
+    for(int j1=1;j1<=i;j1++){
+System.out.print("* ");
+    }
+    for(int j2=1; j2<=i-1; j2++)
+    System.out.print("* ");
+    System.out.println();
+}
+    }
+}/* */
+
+
+
+/*int n=5;
+for(int i=n;i>=1;i--){
+    for(int k=0; k<(n-i); k++){
+        System.out.print("  ");
+    }
+    
+    for(int j1=1;j1<=i;j1++){
+System.out.print("* ");
+    }
+    for(int j2=1; j2<=i-1; j2++)
+    System.out.print("* ");
+    System.out.println();
+}
+    }
+}/* */
+
+/*Right facing triangle
+int n=5;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        System.out.print(" * ");
+       
+    }
+    System.out.println();
+}
+}
+}/* */
+
+
+/*left facing triangle 
+int n=5;
+
+for(int i=1;i<=n;i++){
+    for(int k=0;k<(n-i);k++){
+        System.out.print(" ");
+    }
+    for(int j=1;j<i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+
+System.out.println();
+    }
+}*/
+
+
+
+/*inverted right trianglr 
+int n=5;
+for(int i=n;i>=1;i--){
+    for(int j=1;j<i;j++){
+        System.out.print(" * ");
+    }
+    System.out.println();
+}
+System.out.println();
+    }
+}*/
+
+
+
+/*inverted left triangle
+int n=5;
+for(int i=n;i>=1;i--){
+    for(int k=0;k<(n-i);k++){
+        System.out.print("    ");
+    }
+    for(int j=1;j<i;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+    }
+}*/
+
+/* digonal pattern 
+int n=5;
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        if(i==j || i+j==n-1){
+            System.out.print("*");
+        } else {
+            System.out.print(" ");
+        }
+    }
+    System.out.println();
+}
+    }
+}*/
+
+
+
+
+
+
