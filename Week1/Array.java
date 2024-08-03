c7572f11b30681a8998ac8bf69a711c5ee564f6a
Rishab Kaushik <rishabkaushik125@gmail.com>
2024-08-03 12:07:18 +0530

    array

---
+++ Week1/Array.java
@@ -0,0 +1,130 @@
+
+
+import java.util.Scanner;
+
+/*public class Array {
+
+    public static boolean checkprime(int number ){
+        for(int i=2;i<number;i++)
+        if(number%i==0)
+            return false;
+            return true;
+        }
+        public static void main(String[] args) {
+        int number=3;
+        System.out.print(checkprime(number));    
+        }
+    }*/
+
+
+ /*public static int revrseInteger(int n){
+        int temp=0;
+        while(n!=0){
+            temp=temp*10+n%10;
+            n/=10;
+        }
+        return temp;
+    }
+        
+public static void main(String[] args) {
+    int reverseInteger=4321;
+    System.out.println(reverseInteger(n));
+}
+}*/
+ /*public static int findSum(int Arr[]){
+    int sum=0;
+    for(i=0;i<Arr.length;i++){
+        sum+=Arr[i];
+
+    }
+    return sum;
+}
+public static void main(String[] args) {
+    int [Arr]=new int[10];
+    Scanner sc=new Scanner (System.in);
+    
+
+}}*/
+ /*public static int searchElement(int[] array,int n){
+    for(int i=0;i<array.length;i++){
+        if(array[i]==n){
+            return i;
+        }
+    }
+    return -1;
+}
+public static void main(String[] args) {
+    int[]array={3,4,5,6,7,8,9,10,11};
+    int n=11;
+    int index=searchElement(array,n);
+    if(index!=-1){
+        System.out.println("Element"+n+"found at index"+index);
+
+    } else{
+        System.out.println("Element"+n+"not found in array");
+    }
+
+    
+} 
+    
+}*/
+   /*  public static void reverseArray(int[] array) {
+        int n = array.length;
+        int temp = 0;
+        for (int i = 0; i < n / 2; i++) {
+            temp = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = temp;
+        }
+        for (int i = 0; i < n; i++){
+        System.out.print(array[i]);}
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        reverseArray(arr);
+
+    }
+}*/
+
+
+
+
+
+
+
+
+
+   /*  public static void main(String[] args) {
+        int[] numbers = {3, 5, 7, 2, 8, -1, 4, 10, 12};
+        int max = findMax(numbers);
+        System.out.println("The maximum value in the array is: " + max);
+    }
+
+    public static int findMax(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}*/
+
+
+
+
+
+
+
+
+
+
+
